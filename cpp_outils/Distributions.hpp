127193e767afa2d84ea52a4adda2e88393f4ff32
Administrateur <Administrateur@DESKTOP-J5A3P7L>
2017-07-25 19:40:05 +0200

    ajout classe Distributions

---
+++ cpp_outils/Distributions.hpp
@@ -0,0 +1,21 @@
+/*
+ * Distributions.h
+ *
+ *  Created on: 25 juil. 2017
+ *      Author: Administrateur
+ */
+
+#ifndef DISTRIBUTIONS_HPP_
+#define DISTRIBUTIONS_HPP_
+
+namespace distribution {
+
+class Distributions {
+public:
+	Distributions();
+	virtual ~Distributions();
+};
+
+} /* namespace distribution */
+
+#endif /* DISTRIBUTIONS_HPP_ */
